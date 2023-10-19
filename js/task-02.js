54701aa8b2243787e80eb2cd5281df81c9332bc5
Marius Birsan <marius.birsan90@outlook.com>
2023-10-19 07:23:45 +0300

    task-02 was solved

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const container = document.querySelector('#ingredients');
+
+ingredients.forEach((ingredient) => {
+  const li = document.createElement('li');
+  li.innerText = ingredient;
+  li.classList.add('item');
+  container.append(li);
+})

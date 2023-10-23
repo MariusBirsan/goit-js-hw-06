2a05fc6f395b8d30aa5cbc9e6d8a2f27c231a923
Marius Birsan <marius.birsan90@outlook.com>
2023-10-23 21:00:44 +0300

    task-09 was solved

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+
+const btn = document.querySelector("button");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btn.addEventListener("click", changeStyle);
+
+function changeStyle() {
+  const backgroundColor = document.querySelector(".widget");
+  backgroundColor.style.backgroundColor = getRandomHexColor();
+}
+

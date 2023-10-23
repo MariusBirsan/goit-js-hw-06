e43b8ec636cde9f9318384b4d08e450990dc834c
Marius Birsan <marius.birsan90@outlook.com>
2023-10-23 19:48:15 +0300

    task-08 was saved

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+
+    const user = {
+      email: document.querySelector('input[type="email"]').value,
+      password: document.querySelector('input[type="password"]').value
+    };
+
+    if (user.email === "" || user.password === "") {
+        alert("Please introduce a value in both fields!");
+    } else {
+        console.log(`email: ${user.email}`);
+        console.log(`password: ${user.password}`);
+    }
+
+  loginForm.reset();
+})
+
+
+// console.log();
